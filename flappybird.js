3521c2eba0aeb33ffe0476edf7d17c859c98d01d
cid1228 <samoyloviliya1228@gmail.com>
2026-01-05 21:42:19 +0100

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -6,8 +6,8 @@ let boardHeight = 640;
 let context;
 
 //bird
-let birdWidth = 204; //width/height ratio = 408/228 = 17/12
-let birdHeight = 114;
+let birdWidth = 102; //width/height ratio = 408/228 = 17/12
+let birdHeight = 57;
 let birdX = boardWidth/8;
 let birdY = boardHeight/2;
 let birdImg;
