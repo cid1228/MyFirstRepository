5660653c136f20eeb701d7bb20da25b9dea132a6
cid1228 <samoyloviliya1228@gmail.com>
2026-01-06 14:10:10 +0100

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -63,6 +63,8 @@ window.onload = function() {
     requestAnimationFrame(update);
     setInterval(placePipes, 1500); //every 1.5 seconds
     document.addEventListener("keydown", moveBird());
+    document.addEventListener("click", moveBird());
+
 }
 
 
